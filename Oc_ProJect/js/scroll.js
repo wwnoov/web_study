b892fd6b64e3efd450fd0a8916dc3ec5f1683e95
wwnOOv <145524959+wwnoov@users.noreply.github.com>
2023-10-26 13:08:16 +0900

    Delete Oc_ProJect directory

--- Oc_ProJect/js/scroll.js
+++
@@ -1,9 +0,0 @@
-const headerEl = document.querySelector("header");
-window.addEventListener('scroll', function(){
-  const browerScrollY = window.pageYOffset;
-  if(browerScrollY > 0){
-    headerEl.classList.add("active");
-  }else{
-    headerEl.classList.remove("active");
-  }
-});
